3b5b1a1a4de1ca109895b2763d221ba43da74379
Victor Marques <victor.marques@niara.tech>
2022-01-07 16:57:32 -0300

    api

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,57 +2,39 @@ import { useCallback, useEffect, useState } from "react";
 import apiUsers from "../services/api";
 import { Container } from "./styles";
 
-// import { Container } from './styles';
-
 const Pages: React.FC = () => {
-  const [value, setValue] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState<any>();
 
-  const getUser = useCallback(async (value: string) => {
-    setLoading(true);
+  const getUser = useCallback(async () => {
+    const value = window.location.pathname.substring(1);
 
     try {
-      const { data } = await apiUsers.get(value);
+      const { data } = await apiUsers.get(value === "" ? "otaviojneto" : value);
 
       setUser(data);
     } catch (error) {
       console.log("error", error);
-      setUser(null);
     } finally {
       setLoading(false);
-      setValue("");
     }
   }, []);
 
-  // useEffect(() => {
-  //   getUser("victormarques92");
-  // }, []);
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  console.log("user", user);
+
+  if (loading) {
+    return <h1>Carregando...</h1>;
+  }
 
   return (
     <>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </label>
+      <h1>usuario: {user?.login}</h1>
 
-      <button type="button" onClick={() => getUser(value)}>
-        BUSCAR
-      </button>
-
-      {loading && <h1>Carregando...</h1>}
-
-      {user && !loading && (
-        <>
-          <h1>usuario: {user?.login}</h1>
-
-          <img src={user.avatar_url} alt={user.login} width={200} />
-        </>
-      )}
+      <img src={user?.avatar_url} alt={user.login} width={200} />
     </>
   );
 };
